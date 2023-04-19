8af2e170cb9c3db1073408ec8080d4e6431d495f
stasyurin <stanislav.unn@gmail.com>
2023-04-19 15:37:59 +0300

    getting files content

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -5,14 +5,19 @@ import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
 import picocli.CommandLine.Parameters;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.concurrent.Callable;
+
 @Command(name = "gendiff", mixinStandardHelpOptions = true, version = "gendiff 1.0",
         description = "Compares two configuration files and shows a difference.")
-public class App {
+public class App implements Callable<Integer> {
 
     @Parameters(index = "0", description = "path to first file")
-    private String filepath1;
+    private String filePath1;
     @Parameters(index = "1", description = "path to second file")
-    private String filepath2;
+    private String filePath2;
 
     @Option(names = {"-f", "--format"}, description = "output format [default: ${DEFAULT-VALUE}]")
     private String format = "stylish";
@@ -20,5 +25,21 @@ public class App {
     public static void main(String[] args) {
         int exitCode = new CommandLine(new App()).execute(args);
         System.exit(exitCode);
+    }
+
+    @Override
+    public Integer call() throws Exception {
+        String file1Content = fileContent(filePath1);
+        String file2Content = fileContent(filePath2);
+
+        return 0;
+    }
+
+    private String fileContent(String filePath) throws Exception {
+        Path path = Paths.get(filePath).toAbsolutePath().normalize();
+        if (!Files.exists(path)) {
+            throw new Exception("File '" + path + "' does not exist");
+        }
+        return Files.readString(path);
     }
 }
