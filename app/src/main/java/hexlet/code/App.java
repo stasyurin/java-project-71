16e4c01592699d5fd9f1bb8a98214cdcc85e4fc5
stasyurin <stanislav.unn@gmail.com>
2023-04-24 12:07:46 +0300

    class Differ creating

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,15 +1,10 @@
 package hexlet.code;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
 import picocli.CommandLine.Parameters;
 
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.Map;
 import java.util.concurrent.Callable;
 
 @Command(name = "gendiff", mixinStandardHelpOptions = true, version = "gendiff 1.0",
@@ -31,18 +26,7 @@ public class App implements Callable<Integer> {
 
     @Override
     public Integer call() throws Exception {
-        var file1Data = fileData(filePath1);
-        var file2Data = fileData(filePath2);
-
+        System.out.println(Differ.generate(filePath1, filePath2));
         return 0;
-    }
-
-    private Map<String, Object> fileData(String filePath) throws Exception {
-        Path path = Paths.get(filePath).toAbsolutePath().normalize();
-        if (!Files.exists(path)) {
-            throw new Exception("File '" + path + "' does not exist");
-        }
-        ObjectMapper mapper = new ObjectMapper();
-        return mapper.readValue(path.toFile(), Map.class);
     }
 }
---
+++ app/src/main/java/hexlet/code/Differ.java
@@ -0,0 +1,27 @@
+package hexlet.code;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Map;
+
+public class Differ {
+
+    public static String generate(String filePath1, String filePath2) throws Exception {
+        var file1Data = fileData(filePath1);
+        var file2Data = fileData(filePath2);
+
+        return ;
+    }
+
+    private static Map<String, Object> fileData(String filePath) throws Exception {
+        Path path = Paths.get(filePath).toAbsolutePath().normalize();
+        if (!Files.exists(path)) {
+            throw new Exception("File '" + path + "' does not exist");
+        }
+        ObjectMapper mapper = new ObjectMapper();
+        return mapper.readValue(path.toFile(), Map.class);
+    }
+}
