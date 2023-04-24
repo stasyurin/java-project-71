1b4b596b8498abfe5b9ed411ba93ec79b4fb69e4
stasyurin <stanislav.unn@gmail.com>
2023-04-24 13:25:55 +0300

    generate realisation

--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -5,8 +5,9 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.SortedMap;
+import java.util.TreeMap;
 
 public class Differ {
 
@@ -15,7 +16,47 @@ public class Differ {
         var file2Data = fileData(filePath2);
         var statuses = statuses(file1Data, file2Data);
 
-        return ;
+        var sb = new StringBuilder();
+        sb.append('{');
+        sb.append('\n');
+        for (var kv : statuses.entrySet()) {
+            sb.append("  ");
+            if (kv.getValue().equals("unchanged")) {
+                sb.append("  ");
+                sb.append(kv.getKey());
+                sb.append(": ");
+                sb.append(file1Data.get(kv.getKey()));
+            } else if (kv.getValue().equals("added")) {
+                sb.append("+ ");
+                sb.append(kv.getKey());
+                sb.append(": ");
+                sb.append(file2Data.get(kv.getKey()));
+            } else if (kv.getValue().equals("deleted")) {
+                sb.append("- ");
+                sb.append(kv.getKey());
+                sb.append(": ");
+                sb.append(file1Data.get(kv.getKey()));
+            } else if (kv.getValue().equals("changed")) {
+                sb.append("- ");
+                sb.append(kv.getKey());
+                sb.append(": ");
+                sb.append(file1Data.get(kv.getKey()));
+
+                sb.append('\n');
+                sb.append("  ");
+
+                sb.append("+ ");
+                sb.append(kv.getKey());
+                sb.append(": ");
+                sb.append(file2Data.get(kv.getKey()));
+            } else {
+                throw new Exception("unknown status");
+            }
+            sb.append('\n');
+        }
+        sb.append('}');
+
+        return sb.toString();
     }
 
     private static Map<String, Object> fileData(String filePath) throws Exception {
@@ -27,8 +68,8 @@ public class Differ {
         return mapper.readValue(path.toFile(), Map.class);
     }
 
-    public static Map<String,String> statuses(Map<String, Object> map1, Map<String, Object> map2) {
-        Map<String,String> result = new LinkedHashMap<>();
+    public static SortedMap<String,String> statuses(Map<String, Object> map1, Map<String, Object> map2) {
+        SortedMap<String,String> result = new TreeMap<>();
         for (var key : map1.keySet()) {
             if (map2.get(key) == null) {
                 result.put(key, "deleted");
