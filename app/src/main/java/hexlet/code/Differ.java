0cf17037852f7b85016140db258c4926699e9302
stasyurin <stanislav.unn@gmail.com>
2023-05-03 17:11:25 +0300

    refatoring

--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -10,58 +10,56 @@ import java.util.SortedMap;
 import java.util.TreeMap;
 
 public class Differ {
-    private static final Map<String, Character> STATUSES_SYMBOLS = Map.of(
-            "added", '+',
-            "deleted", '-',
-            "unchanged", ' ');
 
     public static String generate(String filePath1, String filePath2) throws Exception {
         var file1Data = fileData(filePath1);
         var file2Data = fileData(filePath2);
-        var statuses = statuses(file1Data, file2Data);
+        var keyStatuses = keyStatuses(file1Data, file2Data);
 
-        if (statuses.isEmpty()) {
+        if (keyStatuses.isEmpty()) {
             return "{}";
         }
 
+        return buildString(file1Data, file2Data, keyStatuses);
+    }
+
+    private static String buildString(Map<String, Object> file1Data, Map<String, Object> file2Data,
+                                      SortedMap<String, String> keyStatuses) throws Exception {
         var sb = new StringBuilder();
         sb.append('{');
         sb.append('\n');
-        for (var kv : statuses.entrySet()) {
-            sb.append("  ");
-            if (STATUSES_SYMBOLS.containsKey(kv.getValue())) {
-                sb.append(STATUSES_SYMBOLS.get(kv.getValue()));
-                sb.append(" ");
-                sb.append(kv.getKey());
-                sb.append(": ");
-            }
-            if (kv.getValue().equals("unchanged")) {
-                sb.append(file1Data.get(kv.getKey()));
-            } else if (kv.getValue().equals("added")) {
-                sb.append(file2Data.get(kv.getKey()));
-            } else if (kv.getValue().equals("deleted")) {
-                sb.append(file1Data.get(kv.getKey()));
-            } else if (kv.getValue().equals("changed")) {
-                sb.append("- ");
-                sb.append(kv.getKey());
-                sb.append(": ");
-                sb.append(file1Data.get(kv.getKey()));
-
-                sb.append('\n');
-                sb.append("  ");
-
-                sb.append("+ ");
-                sb.append(kv.getKey());
-                sb.append(": ");
-                sb.append(file2Data.get(kv.getKey()));
-            } else {
-                throw new Exception("unknown status");
-            }
-            sb.append('\n');
+        for (var kv : keyStatuses.entrySet()) {
+            appendBlock(sb, kv.getKey(), kv.getValue(), file1Data, file2Data);
         }
         sb.append('}');
 
         return sb.toString();
+    }
+
+    private static void appendBlock(StringBuilder sb, String key, String status, Map<String, Object> file1Data,
+                                    Map<String, Object> file2Data) throws Exception {
+        if (status.equals("unchanged")) {
+            appendLine(sb, ' ', key, file1Data.get(key).toString());
+        } else if (status.equals("added")) {
+            appendLine(sb, '+', key, file2Data.get(key).toString());
+        } else if (status.equals("deleted")) {
+            appendLine(sb, '-', key, file1Data.get(key).toString());
+        } else if (status.equals("changed")) {
+            appendLine(sb, '-', key, file1Data.get(key).toString());
+            appendLine(sb, '+', key, file2Data.get(key).toString());
+        } else {
+            throw new Exception("unknown status");
+        }
+    }
+
+    private static void appendLine(StringBuilder sb, Character symbol, String key, String value) {
+        sb.append("  ");
+        sb.append(symbol);
+        sb.append(" ");
+        sb.append(key);
+        sb.append(": ");
+        sb.append(value);
+        sb.append('\n');
     }
 
     private static Map<String, Object> fileData(String filePath) throws Exception {
@@ -73,23 +71,23 @@ public class Differ {
         return mapper.readValue(path.toFile(), Map.class);
     }
 
-    public static SortedMap<String, String> statuses(Map<String, Object> map1, Map<String, Object> map2) {
-        SortedMap<String, String> result = new TreeMap<>();
+    public static SortedMap<String, String> keyStatuses(Map<String, Object> map1, Map<String, Object> map2) {
+        SortedMap<String, String> keyStatuses = new TreeMap<>();
         for (var key : map1.keySet()) {
             if (map2.get(key) == null) {
-                result.put(key, "deleted");
+                keyStatuses.put(key, "deleted");
             } else {
-                result.put(key, "unchanged");
+                keyStatuses.put(key, "unchanged");
             }
         }
         for (var key : map2.keySet()) {
             var map1Val = map1.get(key);
             if (map1Val == null) {
-                result.put(key, "added");
+                keyStatuses.put(key, "added");
             } else if (!map1Val.equals(map2.get(key))) {
-                result.put(key, "changed");
+                keyStatuses.put(key, "changed");
             }
         }
-        return result;
+        return keyStatuses;
     }
 }
