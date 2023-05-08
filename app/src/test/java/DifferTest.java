816797ab8d2768ef626445061bd8ba2ceaf4065b
stasyurin <stanislav.unn@gmail.com>
2023-05-08 13:27:42 +0300

    yml test common case

--- app/src/test/java/DifferTest.java
+++ app/src/test/java/DifferTest.java
@@ -6,7 +6,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 public final class DifferTest {
 
     @Test
-    void testCommonCase() throws Exception {
+    void testCommonCaseJSON() throws Exception {
         var expected = "{\n"
                 + "  - follow: false\n"
                 + "    host: hexlet.io\n"
@@ -23,6 +23,20 @@ public final class DifferTest {
     void testEmptyJSON() throws Exception {
         var expected = "{}";
         var actual = Differ.generate("./src/test/resources/empty1.json", "./src/test/resources/empty2.json");
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    void testCommonCaseYML() throws Exception {
+        var expected = "{\n"
+                + "  - follow: false\n"
+                + "    host: hexlet.io\n"
+                + "  - proxy: 123.234.53.22\n"
+                + "  - timeout: 50\n"
+                + "  + timeout: 20\n"
+                + "  + verbose: true\n"
+                + "}";
+        var actual = Differ.generate("./src/test/resources/file1.yml", "./src/test/resources/file2.yml");
         assertThat(actual).isEqualTo(expected);
     }
 }
